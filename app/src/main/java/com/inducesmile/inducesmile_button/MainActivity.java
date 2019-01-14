e92dbb3ebe7fe67c7f7c6e736c5a2714243eb80d
Peter <dapetoo@gmail.com>
2019-01-14 19:16:41 +0100

    Button Click Events

---
+++ app/src/main/java/com/inducesmile/inducesmile_button/MainActivity.java
@@ -0,0 +1,27 @@
+package com.inducesmile.inducesmile_button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        //Add OnClickEvents to the button that responds to the user event
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Button is pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
