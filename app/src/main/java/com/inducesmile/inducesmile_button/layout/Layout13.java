dc374c742353f9e1e07ef8d9b45d2bbc56819197
Peter <dapetoo@gmail.com>
2019-02-20 15:10:23 +0100

    Layout Components

--- app/src/main/java/com/inducesmile/inducesmile_button/layout/Layout13.java
+++ app/src/main/java/com/inducesmile/inducesmile_button/layout/Layout13.java
@@ -2,6 +2,9 @@ package com.inducesmile.inducesmile_button.layout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.inducesmile.inducesmile_button.R;
 
@@ -11,5 +14,11 @@ public class Layout13 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout13);
+
+        LinearLayout layout = findViewById(R.id.linearlayout);
+        for(int i=0; i<layout.getChildCount(); i++) {
+            ImageView imageView = (ImageView)layout.getChildAt(i);
+            imageView.setImageResource(R.drawable.notebook);
+        }
     }
 }
---
+++ app/src/main/java/com/inducesmile/inducesmile_button/layout/Layout19.java
@@ -0,0 +1,15 @@
+package com.inducesmile.inducesmile_button.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inducesmile.inducesmile_button.R;
+
+public class Layout19 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout19);
+    }
+}
