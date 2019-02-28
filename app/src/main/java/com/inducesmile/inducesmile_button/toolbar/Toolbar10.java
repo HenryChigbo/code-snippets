163177ac1551a178e7a176194154495f7e12caf0
Peter <dapetoo@gmail.com>
2019-03-01 00:34:29 +0100

    Toolbar Components

---
+++ app/src/main/java/com/inducesmile/inducesmile_button/toolbar/Toolbar10.java
@@ -0,0 +1,15 @@
+package com.inducesmile.inducesmile_button.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inducesmile.inducesmile_button.R;
+
+public class Toolbar10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar10);
+    }
+}
