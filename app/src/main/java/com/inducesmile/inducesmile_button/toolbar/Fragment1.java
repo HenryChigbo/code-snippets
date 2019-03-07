0e3f65c305a51defa8724b78d4a8f8649f0bc805
Peter <dapetoo@gmail.com>
2019-03-07 17:35:43 +0100

    Toolbar Components Completed

---
+++ app/src/main/java/com/inducesmile/inducesmile_button/toolbar/Fragment1.java
@@ -0,0 +1,40 @@
+package com.inducesmile.inducesmile_button.toolbar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.inducesmile.inducesmile_button.R;
+
+public class Fragment1 extends Fragment {
+
+
+    public Fragment1(){
+        //Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_toolbar1, container, false);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_items, menu);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Fragment 1");
+    }
+}
---
+++ app/src/main/java/com/inducesmile/inducesmile_button/toolbar/Fragment2.java
@@ -0,0 +1,39 @@
+package com.inducesmile.inducesmile_button.toolbar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.inducesmile.inducesmile_button.R;
+
+public class Fragment2 extends Fragment {
+
+    public Fragment2(){
+        //Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_toolbar2, container, false);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_toolbar11, menu);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Fragment 2");
+    }
+}
