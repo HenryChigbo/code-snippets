7be4eff1c205ee35f5ecc1b2e97dba8d35e3c871
Peter <dapetoo@gmail.com>
2019-04-20 21:57:54 +0100

    Animation continuation

--- app/src/main/java/com/inducesmile/inducesmile_button/animation/Animation17.java
+++ app/src/main/java/com/inducesmile/inducesmile_button/animation/Animation17.java
@@ -1,7 +1,12 @@
 package com.inducesmile.inducesmile_button.animation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.inducesmile.inducesmile_button.R;
 
@@ -11,5 +16,31 @@ public class Animation17 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation17);
+
+        final ImageView imageView = findViewById(R.id.image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateIntent(imageView);
+            }
+        });
+    }
+
+    public void animateIntent(View view) {
+
+        Intent intent = new Intent(this, Animation1.class);
+
+        // Get the transition name from the string
+        String transitionName = getString(R.string.transition_string);
+        ImageView imageView = findViewById(R.id.image);
+
+        ActivityOptionsCompat options =
+
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this,
+                        imageView,   // Starting view
+                        transitionName    // The String
+                );
+        ActivityCompat.startActivity(this, intent, options.toBundle());
+
     }
 }
--- app/src/main/java/com/inducesmile/inducesmile_button/animation/Animation7.java
+++ app/src/main/java/com/inducesmile/inducesmile_button/animation/Animation7.java
@@ -2,6 +2,10 @@ package com.inducesmile.inducesmile_button.animation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.inducesmile.inducesmile_button.R;
 
@@ -11,5 +15,14 @@ public class Animation7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation7);
+
+        final ImageView imageView = findViewById(R.id.image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.repeat);
+                imageView.startAnimation(animation);
+            }
+        });
     }
 }
